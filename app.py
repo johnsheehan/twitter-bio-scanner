7e97c605bedaa5f55d4236552187796ed947811c
johnsheehan <johnsheehan@gmail.com>
2011-11-13 22:02:52 -0800

    externalize config, added requirements.txt

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 # requires simplejson, oauth2
 from oauthtwitter import OAuthApi
 from time import sleep
+from config import oauth_config
 import oauth2 as oauth
 import re
 
 def main():
     # included some symbols to avoid writing more regex
-    stop_words = "\,:,/,a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,b,be,because,been,but,by,c,can,cannot,could,dear,did,do,does,either,e,else,ever,every,f,for,from,g,get,got,h,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,j,just,k,l,least,let,likely,m,may,me,might,most,must,my,neither,n,no,nor,not,o,of,off,often,oh,on,only,or,other,our,own,p,q,r,rather,s,said,say,says,she,should,since,so,some,t,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,u,us,v,w,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,x,y,yet,you,your,z".split(",")
+    stop_words = ".,\,:,/,a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,b,be,because,been,but,by,c,can,cannot,could,dear,did,do,does,either,e,else,ever,every,f,for,from,g,get,got,h,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,j,just,k,l,least,let,likely,m,may,me,might,most,must,my,neither,n,no,nor,not,o,of,off,often,oh,on,only,or,other,our,own,p,q,r,rather,s,said,say,says,she,should,since,so,some,t,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,u,us,v,w,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,x,y,yet,you,your,z".split(",")
 
     # setup oauth
-    consumer_key = ""
-    consumer_secret = ""
-    token = ""
-    token_secret = ""
+    consumer_key    = oauth_config["consumer_key"]
+    consumer_secret = oauth_config["consumer_secret"]
+    token           = oauth_config["token"]
+    token_secret    = oauth_config["token_secret"]
 
     # retreive followers
     twitter = OAuthApi(consumer_key, consumer_secret, token, token_secret)
---
+++ config.py
@@ -0,0 +1,6 @@
+oauth_config = {
+	"consumer_key": "",
+    "consumer_secret": "",
+    "token": "",
+    "token_secret": ""
+}
